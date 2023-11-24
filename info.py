0c138f56fbf1065dec68e47c39d8fe7227d0377c
telegram209 <151544630+telegram209@users.noreply.github.com>
2023-11-25 02:20:31 +0600

    Update info.py

--- info.py
+++ info.py
@@ -2,7 +2,7 @@ API_ID       = 3145117
 API_HASH     = "f96f01c454431c559ebe903b7c67040e"
 BOT_TOKEN    = "6960428169:AAEB7eepI7X0parmixiwLSa9o30o3Yr86nI"
 SESSION      = "BQAv_Z0AtbzYQwtTH9_BmK-L2d7VxO97EH1N985LGokTOKJGH74VIAPMKI7NDTk1R8uvNzlC3XVS0eACTlKqBc6svfnRuOBYNoBNjHq24QMYXXUZkWWaXcATO6jCXJ7aj-B9FQeveDc_7BInzn3ImMBag35x5AGZyQMDofCDOUz8dgzxoO3S9_-n62-Ip-0486bwt0bKhHII_KWRr3P9DSZ6pkKHs6JT2Fj9_x4sClf-zu4DolRJrOPVZuT_mwXRxTLZ1TiYV-_v9ZWu5wACHN0RUYUbkFrECuWFCPR8qOJdeekoK0z7FoiNEJsq0Bp1W2skakw95iVi3K7iHO-enfi7oa2RRwAAAAFEoSJPAA"
-DATABASE_URI = "mongodb+srv://nayem208:nayem208@cluster0.7l0v3sz.mongodb.net/?retryWrites=true&w=majority"
+DATABASE_URI = "mongodb+srv://nayem2088:nayem2088@cluster0.oy7lk6b.mongodb.net/?retryWrites=true&w=majority"
 LOG_CHANNEL  = -1002002832477
 ADMIN        = 5446378063
 CHANNEL      = "@freepaidcourse11"
